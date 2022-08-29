6bbe1c64577bfa971245ba79311925503ee99b0b
narayanankesavan <87275171+narayanankesavan@users.noreply.github.com>
2022-08-29 21:49:46 +0530

    Delete src directory

--- src/main/java/com/iitr/gl/signupservice/data/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.iitr.gl.signupservice.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
--- src/main/java/com/iitr/gl/signupservice/service/SignupService.java
+++
@@ -1,8 +0,0 @@
-package com.iitr.gl.signupservice.service;
-
-import com.iitr.gl.signupservice.shared.UserDto;
-
-public interface SignupService {
-
-    public UserDto createUser(UserDto userDetails);
-}
