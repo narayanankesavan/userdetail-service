a1822acd5855eac10af9e6f40a69dbbdf337cceb
narayanankesavan <87275171+narayanankesavan@users.noreply.github.com>
2022-09-02 20:12:15 +0530

    Delete src directory

--- src/main/java/com/iitr/gl/userdetailservice/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.iitr.gl.userdetailservice.exception;
-
-public class UnauthorizedException extends RuntimeException {
-
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
